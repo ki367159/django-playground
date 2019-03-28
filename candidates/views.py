1e5ff8ba5a0e1c290588662108f61b69475626e8
kiki <kiki@email.com>
2019-03-28 18:36:55 +0800

    0328增刪改查完

--- candidates/views.py
+++ candidates/views.py
@@ -1,10 +1,12 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
+from django.contrib import messages
 from .models import Candidate
 from .forms import CandidateForm
+from core.forms import DeleteConfirmForm
 
 def index(request):
     candidates = Candidate.objects.all()
-    return render(request, 'candidates_list.html',{
+    return render(request, 'candidates/index.html',{
         'candidates': candidates,
     })
 
@@ -12,7 +14,37 @@ def add(request):
     form = CandidateForm(request.POST or None)
     if form.is_valid():
         form.save()
+        messages.success(request, '新增成功')
         return redirect('/candidates/')
-    return render(request, 'candidate_add.html', {
+    return render(request, 'candidates/add.html', {
         'form': form,
+    })
+
+def edit(request, pk):
+    # candidate = Candidate.objects.get(pk=pk)
+    candidate = get_object_or_404(Candidate, pk=pk)
+    form = CandidateForm(request.POST or None, instance=candidate)
+    if form.is_valid():
+        form.save()
+        messages.success(request, '編輯成功')
+        return redirect('/candidates/')
+    return render(request, 'candidates/edit.html', {
+        'form': form,
+    })
+
+def delete(request, pk):
+    candidate = get_object_or_404(Candidate, pk=pk)
+    form = DeleteConfirmForm(request.POST or None)
+    if form.is_valid():
+        candidate.delete()
+        messages.success(request, '刪除成功')
+        return redirect('/candidates/')
+    return render(request, 'candidates/delete.html', {
+        'form': form,
+    })
+
+def show(request, pk):
+    candidate = get_object_or_404(Candidate, pk=pk)
+    return render(request, 'candidates/show.html', {
+        'candidate': candidate,
     })
--- core/forms.py
+++ core/forms.py
@@ -4,3 +4,6 @@ from django import forms
 class BmiForm(forms.Form):
     h = forms.FloatField(min_value=1, label='身高')
     w = forms.FloatField(min_value=1, label='體重')
+
+class DeleteConfirmForm(forms.Form):
+    check = forms.BooleanField(label='你確定要刪除嗎？')
